7a7dbd587ac5fc3c90094760c24545554c6da933
Viktor Jovanoski <viktor@carvic.si>
2020-05-31 02:59:46 +0200

    playing around with docker

--- docker/create-img.sh
+++ docker/create-img.sh
@@ -1,3 +1,3 @@
-cp ../requirements.txt .
-sed -i  's/>=/==/g' ./requirements.txt
+cat ../requirements.txt | grep -v "tensorflow" > ./requirements.txt
+# sed -i  's/>=/==/g' ./requirements.txt
 docker build . -t bergloman:ad_examples
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 # cd ..
 
 # docker run -it -v $PWD:/home/dev bergloman:ad_examples bash
 
 # docker run -it -v ${PWD}:/home/dev bergloman:ad_examples bash
+
+docker-compose -f docker-compose.yml  up
