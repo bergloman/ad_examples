6f2083dc27f2a21d1f612af77e68e99827c0f71e
Viktor Jovanoski <viktor@carvic.si>
2020-06-05 18:01:33 +0200

    Test

--- docker/flat/test_loda.py
+++ docker/flat/test_loda.py
@@ -13,33 +13,38 @@ from ad_examples.loda.loda import Loda
 
 logger = logging.getLogger(__name__)
 
-# Prepare the aad arguments. It is easier to first create the parsed args and
-# then create the actual AadOpts from the args
-args = get_aad_command_args(debug=True, debug_args=get_debug_args())
+# # Prepare the aad arguments. It is easier to first create the parsed args and
+# # then create the actual AadOpts from the args
+# args = get_aad_command_args(debug=True, debug_args=get_debug_args())
 
 
-opts = AadOpts(args)
-logger.debug(opts.str_opts())
-np.random.seed(opts.randseed)
-# load synthetic (toy 2) dataset
-x, y = get_synthetic_samples(stype=2)
+# opts = AadOpts(args)
+# logger.debug(opts.str_opts())
+# np.random.seed(opts.randseed)
+# # load synthetic (toy 2) dataset
+# x, y = get_synthetic_samples(stype=2)
 
-# run interactive anomaly detection loop
-model, x_transformed, queried, ridxs_counts, region_extents = detect_anomalies_and_describe(x, y, opts)
+# # run interactive anomaly detection loop
+# model, x_transformed, queried, ridxs_counts, region_extents = detect_anomalies_and_describe(x, y, opts)
 
 
 ad_type="loda"
 rnd.seed(42)
 
+print("loading csv...")
 data_df = read_csv("../notebooks/data/simple.type123.csv", header=True)
+print("transforming data...")
 x, y = dataframe_to_matrix(data_df)
 
 n = x.shape[0]
 outliers_fraction = 0.01
 xx = yy = x_grid = Z = scores = None
 
+print("running LODA...")
 ad = Loda(mink=100, maxk=200)
 ad.fit(x)
+
+print("Evaluating...")
 scores = -ad.decision_function(x)
 # Z = -ad.decision_function(x_grid)
 
