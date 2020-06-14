93b1ea4a438e617af1aab80f51729bebd149b60b
Viktor Jovanoski <viktor@carvic.si>
2020-06-14 19:18:45 +0200

    Work

--- docker/flat/test_pandas.py
+++ docker/flat/test_pandas.py
@@ -39,31 +39,39 @@ def slice_data(x, y, idx_from, idx_to):
     return (x[idx_from:idx_to, :], y[idx_from:idx_to])
 
 
-def run_loda(x, y):
-    ad_type="loda"
-    data_type = "type123"
-    data_size="complex"
+def run_loda(x_old, scores_old, x, outliers_fraction):
+    # ad_type="loda"
+    # data_type = "type123"
+    # data_size="complex"
 
     rnd.seed(42)
 
     n = x.shape[0]
-    outliers_fraction = np.sum(y) / len(y)
-    xx = yy = x_grid = Z = scores = None
+    # outliers_fraction = np.sum(y) / len(y)
+    # xx = yy = x_grid = Z = scores = None
 
     print("running LODA...")
     ad = Loda(mink=100, maxk=200)
-    ad.fit(x)
+    ad.fit(x_old)
+    if scores_old == None:
+        print("Calculating inital scores")
+        scores_old = -ad.decision_function(x_old)
 
     print("Evaluating...")
     scores = -ad.decision_function(x)
 
-    print("Converting scores to classes...")
-    y_pred = convert_scores_to_classes(scores, outliers_fraction)
+    print("Combining with historic scores and converting to classes...")
+    print(scores_old)
+    print(scores)
+    scores_combined = np.concatenate(scores_old, scores)
+    y_pred_combined = convert_scores_to_classes(scores_combined, outliers_fraction)
+    y_pred = y_pred_combined[len(scores_old):]
 
-    print("Calculating F1 scores...")
-    f1 = f1_score(y, y_pred, average=None) # average='weighted')
-    # print("F1={:f}".format(f1))
-    print(f1)
+    # print("Calculating F1 scores...")
+    # f1 = f1_score(y, y_pred, average=None) # average='weighted')
+    # print(f1)
+
+    return (scores_combined, y_pred)
 
 # print(x)
 
@@ -81,16 +89,26 @@ def run_loda(x, y):
 (gt_x, gt_y) = load_data()
 # print(gt_x.shape)
 
+day_rec_cnt = 24 * 12
+block_size = 70 * day_rec_cnt
+idx_start = 160 * day_rec_cnt
+idx_curr_time = idx_start
+n = gt_y.shape[0]
+scores_all = None
+y_pred = np.zeros(0)
 
-(gt_x2, gt_y2) = slice_data(gt_x, gt_y, 0, 20000)
-run_loda(gt_x2, gt_y2)
+while idx_curr_time < n :
+    print(n, idx_curr_time, block_size)
+    (x1, y1) = slice_data(gt_x, gt_y, 0, idx_curr_time)
+    (x2, y2) = slice_data(gt_x, gt_y, idx_curr_time, idx_curr_time + block_size)
+    (scores_all, y_pred_new) = run_loda(x1, scores_all, x2, 0.01)
+    y_pred = np.concatenate((y_pred, y_pred_new), 0)
+    idx_curr_time = idx_curr_time + block_size
 
-(gt_x3, gt_y3) = slice_data(gt_x, gt_y, 0, 50000)
-run_loda(gt_x3, gt_y3)
+print("finished with training, analyzing combined output")
+y = gt_y[idx_start:]
 
-# print(gt_x2.shape)
-# print(gt_y2.shape)
-# y1 = gt_y[0:10]
-# y2 = gt_y[10:12]
-# print(y1)
-# print(y2)
+print("Calculating F1 scores...")
+f1 = f1_score(y, y_pred, average=None) # average='weighted')
+
+print(f1)
