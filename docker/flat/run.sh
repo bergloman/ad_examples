ea7a30e5182b2f3aa034fdcfe3a5e4c539b55224
bergloman <bergloman@gmail.com>
2020-08-24 11:45:04 +0200

    better execution script

--- docker/flat/run.sh
+++ docker/flat/run.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-ALGOS=("loda" "lof" "ifor")
-TYPES=("type-b" "type-e" "type-r" "type-be" "type-br" "type-er" "type-ber")
-SOURCES=("huge" "complex" "simple" "single")
-DATA_TYPES=("csv" "csv_normalized_hours")
+#ALGOS=("loda" "lof" "ifor")
+# TYPES=("type-b" "type-e" "type-r" "type-be" "type-br" "type-er" "type-ber")
+# SOURCES=("huge" "complex" "simple" "single")
+# DATA_TYPES=("csv" "csv_normalized_hours")
 
-# ALGOS=("ifor")
-# TYPES=("type-ber")
-# SOURCES=("single")
+ALGOS=("loda")
+TYPES=("type-ber")
+SOURCES=("single")
+DATA_TYPES=("csv_normalized_hours")
 
 OUTPUT_DIR=out
 mkdir -p $OUTPUT_DIR
@@ -18,6 +19,7 @@ do
     do
         DIR=data/data_parking/$DATA_TYPE/$TYPE
         mkdir -p $DIR
+        PIDS=""
 
         for SOURCE in "${SOURCES[@]}"
         do
@@ -26,9 +28,13 @@ do
             do
                 OUTPUT_FILE=$OUTPUT_DIR/$DATA_TYPE.$SOURCE.$TYPE.$ALGO.txt
                 # rm $OUTPUT_FILE
-                python run_simple.py $INPUT_FILE $ALGO > $OUTPUT_FILE
+                python run_simple.py $INPUT_FILE $ALGO > $OUTPUT_FILE &
+                PIDS="$PIDS $!"
             done
         done
+        echo "Waiting for batch of child processes: $PIDS"
+        wait $PIDS
+        echo "Batch of child processes finished ($DATA_TYPE $TYPE $SOURCE)"
     done
 done
 
