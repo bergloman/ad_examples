5adeb842859f95299bedc0d00f42e3e542ba72e0
bergloman <bergloman@gmail.com>
2020-08-28 19:10:12 +0200

    work

--- docker/flat/extract_parking.js
+++ docker/flat/extract_parking.js
@@ -3,23 +3,21 @@
 /* jshint esversion: 6 */
 /* jshint node: true */
 
-/********************************************************************************
- * This utility is used for extracting data from output of experiments.
- */
-
-
 const fs = require("fs");
 
 const sizes = ["single", "simple", "complex", "huge"];
 const types = ["type-b", "type-e", "type-r", "type-be", "type-br", "type-er", "type-ber"];
 const algos = ["loda", "lof", "ifor"];
+const csv_type = "csv";
+//const csv_type = "csv_normalized_hours";
+const decimals = 3;
 
 const res = [];
 for (const type of types) {
     for (const size of sizes) {
-        let s = `${type} & ${size} `;
+        let s = `${type} & ${size}`;
         for (const algo of algos) {
-            const fname = `./csv_normalized_hours.${size}.${type}.${algo}.txt`;
+            const fname = `./${csv_type}.${size}.${type}.${algo}.txt`;
             const content = fs.readFileSync(fname, { encoding: "utf8" })
                 .split("\n")
                 .map(x => x.trim())
@@ -27,13 +25,13 @@ for (const type of types) {
                 .pop();
             // console.log(fname, content)
             try {
-                const data = JSON.parse(content);
-                const precision = data.precision;
-                const recall = data.recall;
-                const f1 = data.f1;
-                const f05 = data.f05;
-                res.push({ size, type, algo, f1, precision, recall, f05 });
-                s += `& ` + f1.toFixed(4);
+                const f1 = JSON.parse(content).f1;
+                const precision = JSON.parse(content).precision;
+                const recall = JSON.parse(content).recall;
+                res.push({ size, type, algo, f1 });
+                s += ` & ` + precision.toFixed(decimals);
+                s += ` & ` + recall.toFixed(decimals);
+                s += ` & ` + f1.toFixed(decimals);
             } catch (e) {
                 s += `& ERROR `;
             }
@@ -41,6 +39,7 @@ for (const type of types) {
         s += " \\\\";
         console.log(s);
     }
+    console.log("\\midrule");
 }
 
 // for (const r of res) {
